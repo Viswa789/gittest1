52a68f579aab72ed5c8d26b69268355c60a8b84c
viswa <viswa@viswa-PC>
2021-04-26 17:20:09 +0530

    branch(1) commit 1

---
+++ src/test1/t2.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class t2 {
+
+	public static void main(String[] args) {
+		int n=20;
+		if(n !=0) {
+		System.out.println("new test"+n*28);
+		}
+		else {System.out.println("nullllllllllllllllllllll---means zerooo");}
+	}
+
+}
