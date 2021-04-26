d0b26f57f0a8885e9df9c056364788ad2bf92dd2
Viswa <Viswa@DESKTOP-KBEBKCQ>
2021-04-26 15:46:34 +0530

    first commit------1

---
+++ src/test1/t1.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class t1 {
+
+	public static void main(String[] args) {
+		String new1="-one";
+		if(new1 != null) {
+		System.out.println("new test"+new1);
+		}
+		else {System.out.println("nullllllllllllllllllllll---means zerooo");}
+	}
+
+}
